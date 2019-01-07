3c7816e7bd81b8012ce67a5e934f978bad35651a
mamireimu <support@mamireimuserver.com>
2019-01-07 19:04:29 +0900

    機能追加と各種位置の修正
    ・英訳と追記のチェックボックス追加
    ・英訳の追加

--- Out-of-office confirmation software/Form1.Designer.cs
+++ Out-of-office confirmation software/Form1.Designer.cs
@@ -28,11 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button1
@@ -83,7 +88,7 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button4.Location = new System.Drawing.Point(192, 10);
+            this.button4.Location = new System.Drawing.Point(254, 10);
             this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(54, 25);
@@ -92,17 +97,62 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(192, 10);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(54, 25);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "仕事中";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(317, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(51, 22);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "改行";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(317, 45);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(51, 22);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "英訳";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(257, 45);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(51, 22);
+            this.checkBox3.TabIndex = 8;
+            this.checkBox3.Text = "追記";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(255, 77);
+            this.ClientSize = new System.Drawing.Size(366, 75);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Out-of-office confirmation software";
@@ -118,6 +168,10 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
     }
 }
 
--- Out-of-office confirmation software/Form1.cs
+++ Out-of-office confirmation software/Form1.cs
@@ -13,29 +13,128 @@ namespace Out_of_office_confirmation_software
 {
     public partial class Form1 : Form
     {
+        string out_file="./status.txt";
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        //在席中のボタン動作
         private void button1_Click(object sender, EventArgs e)
         {
-            File.WriteAllText(@".\status.txt", "在席中");
+            File.WriteAllText(@out_file, "在席中");
+
+            //英訳判定
+            if (checkBox2.Checked)
+            {
+                //改行判定
+                if (checkBox1.Checked)
+                {
+                    File.AppendAllText(@out_file, Environment.NewLine);
+                }
+                File.AppendAllText(@out_file, "[In table that caught]");
+            }
+
+            //追記判定
+            if (checkBox3.Checked)
+            {
+                //改行判定
+                if (checkBox1.Checked)
+                {
+                    File.AppendAllText(@out_file, Environment.NewLine);
+                }
+                File.AppendAllText(@out_file, textBox1.Text);
+            }
         }
 
+        //退席中のボタン動作
         private void button2_Click(object sender, EventArgs e)
         {
-            File.WriteAllText(@".\status.txt", "退席中");
+            File.WriteAllText(@out_file, "退席中");
+
+            //英訳判定
+            if (checkBox2.Checked)
+            {
+                //改行判定
+                if (checkBox1.Checked)
+                {
+                    File.AppendAllText(@out_file, Environment.NewLine);
+                }
+                File.AppendAllText(@out_file, "[away from the keyboard]");
+            }
+
+            //追記判定
+            if (checkBox3.Checked)
+            {
+                //改行判定
+                if (checkBox1.Checked)
+                {
+                    File.AppendAllText(@out_file, Environment.NewLine);
+                }
+                File.AppendAllText(@out_file, textBox1.Text);
+            }
         }
 
+        //外出中のボタン動作
         private void button3_Click(object sender, EventArgs e)
         {
-            File.WriteAllText(@".\status.txt", "外出中");
+            File.WriteAllText(@out_file, "外出中");
+
+            //英訳判定
+            if (checkBox2.Checked)
+            {
+                //改行判定
+                if (checkBox1.Checked)
+                {
+                    File.AppendAllText(@out_file, Environment.NewLine);
+                }
+                File.AppendAllText(@out_file, "[Now absent]");
+            }
+
+            //追記判定
+            if (checkBox3.Checked)
+            {
+                //改行判定
+                if (checkBox1.Checked)
+                {
+                    File.AppendAllText(@out_file, Environment.NewLine);
+                }
+                File.AppendAllText(@out_file, textBox1.Text);
+            }
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            File.WriteAllText(@".\status.txt", textBox1.Text);
+            File.WriteAllText(@out_file, textBox1.Text);
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(@out_file, "仕事中");
+
+            //英訳判定
+            if (checkBox2.Checked)
+            {
+                //改行判定
+                if (checkBox1.Checked)
+                {
+                    File.AppendAllText(@out_file, Environment.NewLine);
+                }
+                File.AppendAllText(@out_file, "[working now]");
+            }
+
+            //追記判定
+            if (checkBox3.Checked)
+            {
+                //改行判定
+                if (checkBox1.Checked)
+                {
+                    File.AppendAllText(@out_file, Environment.NewLine);
+                }
+                File.AppendAllText(@out_file, textBox1.Text);
+            }
+        }
+
     }
 }
